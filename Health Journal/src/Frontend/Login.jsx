0cf0516036bf9ff0ea0fb7929cece15a87fe79e9
Isaiaarasan <arasan9706@gmail.com>
2025-01-30 22:13:53 +0530

    backend

--- Health Journal/src/Frontend/Login.jsx
+++ Health Journal/src/Frontend/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
         setError('');
         
         try {
-            const response = await fetch('https://health-journal-project-3.onrender.com/api/login', {
+            const response = await fetch('/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
--- Health Journal/src/Frontend/Signup.jsx
+++ Health Journal/src/Frontend/Signup.jsx
@@ -13,7 +13,7 @@ const Signup = () => {
         setError('');
 
         try {
-            const response = await fetch('https://health-journal-project-3.onrender.com/api/signup', {
+            const response = await fetch('/api/signup', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
