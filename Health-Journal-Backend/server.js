2672edab59058ca099e23d403e767374b3ecd355
ELAVARASAN P <149961332+Isaiaarasan@users.noreply.github.com>
2025-01-30 23:03:17 +0530

    Update server.js

--- Health-Journal-Backend/server.js
+++ Health-Journal-Backend/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-    origin: '', 
+    origin: 'https://health-journal-project-3.vercel.app/', 
     credentials: true
 }));
 app.use(express.json());
