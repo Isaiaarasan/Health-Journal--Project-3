960135c33443f6d54bbd44b2c7886d27aa9f5a4a
ELAVARASAN P <149961332+Isaiaarasan@users.noreply.github.com>
2025-01-30 23:13:46 +0530

    Update Login.jsx

--- Health Journal/src/Frontend/Login.jsx
+++ Health Journal/src/Frontend/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
         setError('');
         
         try {
-            const response = await fetch('https://health-journal-project-3-api.vercel.app/api/login', {
+            const response = await fetch('https://health-journal-project-3-api.vercel.app/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
